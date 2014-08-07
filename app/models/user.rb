ea7042317e3cc3bb71f6b180d4cf983c1db7beee
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-08-07 22:09:14 +0800

    Disable email login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
   devise :omniauthable, :omniauth_providers => [:facebook, :google_oauth2, :twitter]
   validates :name, presence: true
