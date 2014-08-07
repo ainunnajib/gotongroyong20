6d276e3e348cd9d0d926c6c5c0009a12fc88e299
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-08-07 22:04:27 +0800

    Fix User logic for Twitter & Google

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,12 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
-      user.email = auth.info.email
+      if auth.info.email
+        user.email = auth.info.email
+      else
+        user.email = SecureRandom.hex + "@levifikri.com"
+      end
+
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name
       user.image = auth.info.image
@@ -19,6 +24,10 @@ class User < ActiveRecord::Base
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
+      elsif data = session["devise.google_data"] && session["devise.google_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      elsif data = session["devise.twitter_data"] && session["devise.twitter_data"]["extra"]["raw_info"]
+        user.email = SecureRandom.hex + "@levifikri.com" if user.email.blank?
       end
     end
   end
