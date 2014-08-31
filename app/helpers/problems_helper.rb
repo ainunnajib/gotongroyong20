1b388968827d4e0e07487cf810c4cb9a693cc1e0
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-08-31 22:24:41 +0800

    Project problems into Google Map

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -1,2 +1,9 @@
 module ProblemsHelper
+  def long_lat_points(problems)
+    res = []
+    problems.each do |problem|
+      res.append([problem.kecamatan.latitude, problem.kecamatan.longitude])
+    end
+    return res.to_json
+  end
 end
