03101b2ea4ad95382fe3f90c6fd33ba24700500f
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-08-31 21:10:31 +0800

    Add all_categories and all_urgencies to problem

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,2 +1,11 @@
 class Problem < ActiveRecord::Base
+  belongs_to :reported_by, class_name: "User"
+
+  def self.all_categories
+    return [["Kesehatan", 0], ["Pendidikan", 1], ["Hukum", 2]]
+  end
+
+  def self.all_urgencies
+    return [["Sangat rendah", 0], ["Rendah", 1], ["Menengah", 2], ["Penting", 3], ["Kritikal", 4]]
+  end
 end
