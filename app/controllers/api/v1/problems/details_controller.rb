33d19712dbb0125954976b7d97c5de0efaca39c2
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-09-04 14:12:30 +0800

    Display 20 problems per page

--- app/controllers/api/v1/problems/details_controller.rb
+++ app/controllers/api/v1/problems/details_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::Problems::DetailsController < ApplicationController
   def index
-    @problems = Problem.includes([:reported_by, :kecamatan]).paginate(page: params[:page], per_page: 2)
+    @problems = Problem.includes([:reported_by, :kecamatan]).paginate(page: params[:page], per_page: 20)
 
     @problems = @problems.where(province_id: params[:province_id].to_i) if params[:province_id]
     @problems = @problems.where(kabupaten_id: params[:kabupaten_id].to_i) if params[:kabupaten_id]
