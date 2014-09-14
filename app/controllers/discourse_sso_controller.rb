ce510eb2a4c78a66ad37d1541a5c8ca303ae37d1
Pahlevi Fikri Auliya <pahlevi.fikri.auliya@gmail.com>
2014-09-14 17:27:05 +0800

    Bug fix: if the user is not signed in, just display the main page without authentication

--- app/controllers/discourse_sso_controller.rb
+++ app/controllers/discourse_sso_controller.rb
@@ -3,18 +3,29 @@ require 'uri'
 
 class DiscourseSsoController < ApplicationController
   def sso
-    secret = "1qazxcde345"
-    sso = SingleSignOn.parse(request.query_string, secret)
-    sso.email = current_user.email
-    sso.name = current_user.name
-    sso.username = current_user.name
-    sso.external_id = current_user.id
-    sso.sso_secret = secret
+    regex_group = /.+\/(\d+)\.html/.match(request.referrer)
+    if regex_group
+      problem_id = regex_group.captures[0]
+    else
+      # Should be avoided to go here by disabling login from Discourse
+      redirect_to "http://www.discourse.gotong.royong.org"
+      return
+    end
 
-    # http://localhost:3000/problems/9501.html
-    # Hack: Extract 9501
+    if user_signed_in?
+      secret = "1qazxcde345"
+      sso = SingleSignOn.parse(request.query_string, secret)
+      sso.email = current_user.email
+      sso.name = current_user.name
+      sso.username = current_user.name
+      sso.external_id = current_user.id
+      sso.sso_secret = secret
 
-    problem_id = /.+\/(\d+)\.html/.match(request.referrer).captures[0]
-    redirect_to sso.to_url("http://#{problem_id}.discourse.gotong.royong.org/session/sso_login")
+      # http://localhost:3000/problems/9501.html
+      # Hack: Extract 9501
+      redirect_to sso.to_url("http://#{problem_id}.discourse.gotong.royong.org/session/sso_login")
+    else
+      redirect_to "http://#{problem_id}.discourse.gotong.royong.org/"
+    end
   end
 end
